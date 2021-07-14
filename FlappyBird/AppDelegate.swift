eaed87b2b3479777719ff9f4ecd22f7ba80f7bd6
akiyoshi220 <ak220@hotmail.com>
2021-07-14 21:39:01 +0900

    finished L7C6

--- FlappyBird/AppDelegate.swift
+++ FlappyBird/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  FlappyBird
 //
-//  Created by 小池　亮義 on 2021/07/14.
+//  Created by akiyoshi220 on 2021/07/14.
 //
 
 import UIKit
--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -2,16 +2,27 @@
 //  ViewController.swift
 //  FlappyBird
 //
-//  Created by 小池　亮義 on 2021/07/14.
+//  Created by akiyoshi220 on 2021/07/14.
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // SKViewに型を変換する
+        let skView = self.view as! SKView
+        // FPSを表示する
+        skView.showsFPS = true
+        // ノードの数を表示する
+        skView.showsNodeCount = true
+        // ビュート同じサイズでシーンを作成する
+        let scene = GameScene(size: skView.frame.size)
+        // ビューにシーンを表示する
+        skView.presentScene(scene)
     }
 
 
